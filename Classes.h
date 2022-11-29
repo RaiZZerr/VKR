86f9f25579d14a3fe71694056872b0362b378ec7
RaiZZerr <raizzerr@gmail.com>
2022-11-29 19:54:29 +0300

    Start

---
+++ Classes.h
@@ -0,0 +1,20 @@
+
+#include <vector>
+#include <random>
+
+using namespace std;
+
+class Surface {
+public:
+	vector <float> x;
+	vector <float> y;
+	vector <float> z;
+	void CreateSurface();
+	void GetCSV();
+
+};
+
+void CreateSurface()
+{
+
+}
