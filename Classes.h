24a701d05c1c01d0e025514a1550714ff2f56648
RaiZZerr <raizzerr@gmail.com>
2022-12-04 00:30:59 +0300

    VKR

--- Classes.h
+++ Classes.h
@@ -24,6 +24,16 @@ class Surface {
 
 };
 
+class Sphere {
+	public:
+		float r;
+		vector <float> x;
+		vector <float> y;
+		vector <float> z;
+		void CreateSphere();
+		void GetCSV();
+};
+
 void Surface::CreateSurface()
 {
 
@@ -33,23 +43,110 @@ void Cylinder::CreateCylinder()
 {
 	random_device rd;
 	mt19937 gen(rd());
-	uniform_real_distribution<> dist(1, 100);
+	uniform_real_distribution<> dist(5, 100);
 	h = dist(gen);
 	r = dist(gen);
 	// x = r*cos(fi), y = r * sin(fi)
  
-	// Генерация основания цилиндра
-	for (float hlpr = r; hlpr > 0; hlpr -= 10)
+	// Генерация основания и вершины цилиндра
+	for (float hlp_r = r; hlp_r > 0; hlp_r -= 10)  // изменение радиуса круга для заполнения основания
 	{
-		for (int i = 0; i <= 360; i += 5)
+		for (int i = 0; i <= 360; i += 20)
 		{
-			x.push_back(hlpr* cos(i));
-			z.push_back(hlpr * sin(i));
+			x.push_back(hlp_r* cos(i));
 			y.push_back(0);
+			z.push_back(hlp_r * sin(i));
+			
+		}
+	}
+
+	for (float hlp_r = r; hlp_r > 0; hlp_r -= 10)  // изменение радиуса круга для заполнения вершины
+	{
+		for (int i = 0; i <= 360; i += 20)
+		{
+			x.push_back(hlp_r * cos(i));
+			y.push_back(h);
+			z.push_back(hlp_r * sin(i));
+			
 		}
 	}
 	//---------------------------------------
 	
 	// Генерация стенок цилиндра
-	while ( <= h)
+	float hlp_y = 0;
+	float k = 0;
+	while (hlp_y < h)
+	{
+		uniform_real_distribution<> dist(5, 10);
+		k = dist(gen);
+		hlp_y += k;
+		for (int i = 0; i <= 360; i += 20)
+		{
+			x.push_back(r * cos(i));
+			y.push_back(hlp_y);
+			z.push_back(r * sin(i));
+		}		
+	}
+}
+
+void Sphere::CreateSphere()
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_real_distribution<> dist(5, 100);
+	r = dist(gen);
+
+	// Генерация образующей окружности сферы
+	for (int i = 0; i <= 360; i += 20)
+	{
+		x.push_back(r * cos(i));
+		y.push_back(0);
+		z.push_back(r * sin(i));
+	}
+	//--------------------------------------
+
+	// Генерация верхней полусферы
+	float hlp_y = 0;
+	float hlp_r = 0;
+	while (hlp_y < r)
+	{
+		uniform_real_distribution<> dist(5, 10);
+		hlp_y += dist(gen);
+		hlp_r = sqrt(r * r - hlp_y * hlp_y);
+		for (int i = 0; i <= 360; i += 20)
+		{
+			x.push_back(hlp_r * cos(i));
+			y.push_back(hlp_y);
+			z.push_back(hlp_r * sin(i));
+		}
+	}
+	//----------------------------------------
+
+	// Генерация нижней полусферы
+	hlp_y = 0;
+	hlp_r = 0;
+	while ((-hlp_y) < r)
+	{
+		uniform_real_distribution<> dist(5, 10);
+		hlp_y -= dist(gen);
+		hlp_r = sqrt(r * r - ((-hlp_y) * (-hlp_y)));
+		for (int i = 0; i <= 360; i += 20)
+		{
+			x.push_back(hlp_r * cos(i));
+			y.push_back(hlp_y);
+			z.push_back(hlp_r * sin(i));
+		}
+	}
+	//----------------------------------------
+
+}
+
+void Cylinder::GetCSV()
+{
+
+}
+
+void Surface::GetCSV()
+{
+
 }
--- VKR.cpp
+++ VKR.cpp
@@ -6,43 +6,20 @@
 
 using namespace std;
 
-
-bool yo[1001][1001] = { false };
-
-int Surface()
-{
-	int x, y, z;
-	z = 0;
-	srand(time(0));
-
-	for (int i = 0; i <= 100000; i++)
-	{
-		x = rand() % 1001;
-		y = rand() % 1001;
-		yo[y][x] = true;
-		cout << x << ',' << y << ',' << z << ' ';
-	}
-
-	return 1;
-}
-
-void see() {
-	for (int i = 0; i <= 1000; i++) {
-		for (int j = 0; j <= 1000; j++) {
-			if (yo[i][j]) cout << "*";
-			else
-				cout << " ";
-		}
-		cout << "\n";
-	}
-}
-
 int main()
 {
 	Cylinder num;
 	num.CreateCylinder();
+	cout << "Cylinder: " << endl;
 	for (int i = 0; i < num.x.size(); i++) {
-		cout << num.x[i] << " , " << num.z[i] << "  ";
+		cout << num.x[i] << " , " << num.y[i] << " , " << num.z[i] << " ;; ";
+	}
+	cout << endl;
+	Sphere cum;
+	cum.CreateSphere();
+	cout << "Sphere: " << endl;
+	for (int j = 0; j < cum.x.size(); j++) {
+		cout << cum.x[j] << " , " << cum.y[j] << " , " << cum.z[j] << " ;; ";
 	}
 	return 0;
 }
